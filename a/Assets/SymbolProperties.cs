b09b66f6e6f0d4b56a12cc6a91c5c47750da4219
FrostedIA <59926444+FrostedIA@users.noreply.github.com>
2020-12-19 20:45:56 +0100

    First_Conceptions
    a per cambiar de mode
    z per deixar anar els objectes

---
+++ a/Assets/SymbolProperties.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymbolProperties : MonoBehaviour
+{
+   // Aquest script serveix per detectar que quan cliquis el simbol perseguiexi el cursor. Per deixar anar l'objecte tecla z
+    public bool Have;
+    public GameObject Him;
+   
+    void Start()
+    {
+        Have = false;
+        transform.localScale = new Vector3(1, 1, 0);
+       
+    }
+
+   
+    public void SetMousePos()
+    {
+        Have = !Have;
+        
+        
+       
+
+    }
+    private void FixedUpdate()
+    {
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Have == true)
+        {
+ 
+        Him.transform.position = worldPosition;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Z) && Have == true)
+        {
+           Have = false;
+        } 
+    }
+   
+}
---
+++ a/Assets/UIController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    //a per cambiar de mode,ho indica en el text
+    public GameObject[] Symbol;
+    public GameObject Lienzo;
+    public int Num;
+    public Text Indicator;
+    public int Item_Num;
+    public bool Swipe;
+
+    void Start()
+    {
+        Indicator.text = "Click";
+        Swipe = false;
+        Item_Num = 0;
+        Num = 0;
+    }
+    public void SetSymbol(int A)
+    {
+        Num = A;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0) && Item_Num ==1)
+        {
+            var NewSymbol = Instantiate(Symbol[Num],worldPosition, Quaternion.identity);
+            NewSymbol.transform.SetParent(Lienzo.transform);
+            Num = 0;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Swipe = !Swipe;
+            if (Swipe == true) 
+            {
+                Item_Num = 1;
+                Indicator.text = "SymbolAdder";
+            }
+            if (Swipe == false)
+            {
+                Item_Num = 0;
+                Indicator.text = "Click";
+            }
+
+
+        }
+        
+    }
+}
